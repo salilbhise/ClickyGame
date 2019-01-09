ff73dc82a9a3d68f923e52f4ed68bd697c9da715
Your Name <salil_bhise@yahoo.com>
2019-01-08 21:56:25 -0600

    wrapper and styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import Card from './Components/Card/Card';
 
-const characters = [
-  {
+
+const characters = [{
     id: 0,
-    name: 'thanos',
-    img: 'https://cdn.vox-cdn.com/thumbor/Gba6nO4uV6xw_iOP8tnQI-YxfyA=/0x0:2048x1080/1200x800/filters:focal(571x183:897x509)/cdn.vox-cdn.com/uploads/chorus_image/image/60706099/thanos.0.jpg'
+    name: 'Justice League',
+    img: 'https://icdn2.digitaltrends.com/image/justice-league-batman.jpg'
   },
   {
     id: 1,
-    name: 'spider man',
-    img: 'https://insomniac.games/wp-content/uploads/2018/09/Spider-Man_PS4_Selfie_Photo_Mode_LEGAL.jpg'
+    name: 'Batman',
+    img: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/09/27/14/batman-1989.jpg?w968h681'
   },
   {
-    id: 2, 
-    name: 'iron man',
-    img: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/Iron_Man_bleeding_edge.jpg/250px-Iron_Man_bleeding_edge.jpg'
-  }
+    id: 2,
+    name: 'Batman and Robin',
+    img: 'http://assets1.ignimgs.com/2014/10/13/clooneybatmanjpg-80068a_1280w.jpg'
+  },
+  {
+    id: 3,
+    name: 'Batman Forever',
+    img: 'https://vignette.wikia.nocookie.net/batman/images/b/b8/Batman-Forever-batman-forever-23673181-640-384.png/revision/latest?cb=20170605105614'
+  },
+  {
+    id: 4,
+    name: 'Batman Begins',
+    img: 'https://pmcvariety.files.wordpress.com/2013/08/batman-begins-christian-bale.jpg?w=1000&h=563&crop=1'
+  },
+
 ]
 
 class App extends Component {
@@ -29,51 +42,71 @@ class App extends Component {
     score: 0
   }
   handleClick = (id) => {
-    if(!this.state.clicked.includes(id)){
+    if (!this.state.clicked.includes(id)) {
       const newArray = this.shuffle(this.state.characters);
-        this.setState({
-          clicked: [...this.state.clicked, id],
-          score: this.state.score + 1,
-          characters: newArray
-        })
-    }else {
+      this.setState({
+        clicked: [...this.state.clicked, id],
+        score: this.state.score + 1,
+        characters: newArray
+      })
+    } else {
       alert("you lose")
     }
   }
   shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
+    var currentIndex = array.length,
+      temporaryValue, randomIndex;
+
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-  
+
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-  
+
       // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
     }
-  
+
     return array;
   }
 
+
+
   render() {
-    return (
-      <div className="App">
-      <Header score={this.state.score}/>
-      <div className="content">
-       {
-         this.state.characters.map( char => {
-           return <Card handleClick={this.handleClick} id={char.id} image={char.img} name={char.name}/>
-         })
-       }
-      </div>
-  
-      </div>
+    return ( <
+      div className = "App" >
+      <
+      Header score = {
+        this.state.score
+      }
+      /> <
+      div className = "content" > {
+        this.state.characters.map(char => {
+          return <Card handleClick = {
+            this.handleClick
+          }
+          id = {
+            char.id
+          }
+          image = {
+            char.img
+          }
+          name = {
+            char.name
+          }
+          />
+        })
+      } <
+      /div>
+
+      <
+      /div>
     );
   }
+
 }
 
 export default App;
